3037caf3a71e78302fccd674da4a674d17b34a66
dpssarathy1418 <parthasarathy.d@mitrahsoft.com>
2024-03-12 19:44:53 +0530

    updated files

--- app/code/Mitrahsoft/Testing/registration.php
+++ app/code/Mitrahsoft/Testing/registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
 	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Mitrahsoft_Helloworld',
+	'Mitrahsoft_Testing',
 	__DIR__
 );
